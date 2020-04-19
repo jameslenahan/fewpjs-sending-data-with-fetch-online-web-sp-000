f004341ea4995b97bd948cc31f227c4f1c6f6630
James Lenahan <james.k.lenahan@gmail.com>
2020-04-19 17:17:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
+import { create } from "domain";
+
 // Add your code here
+
+
+function submitData(userName, userEmail){
+    
+    let formData = {
+        name: userName,
+        email: userEmail
+    }
+    
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    };
+
+    return fetch('http://localhost:3000/users', configObj)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(object){
+        createElementAndAppendToBody(object.id);
+    })
+    .catch(function(error){
+        createElementAndAppendToBody(error.message);
+    })
+}
